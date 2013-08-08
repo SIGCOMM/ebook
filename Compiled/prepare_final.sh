1893141821c77faec26445b0d2267f6cb9ecfa73
haddadi <h.haddadi@gmail.com>
2013-08-08 12:00:33 -0400

    added final chapters

---
+++ Compiled/prepare_final.sh
@@ -0,0 +1,10 @@
+cp ebook.pdf chapters4web/SIGCOMMeBook2013v1.pdf
+cp chapter1/chapter1.pdf    chapters4web/SIGCOMMeBook2013v1_chapter8.pdf
+cp chapter2/chapter2.pdf    chapters4web/SIGCOMMeBook2013v1_chapter4.pdf
+cp chapter3/chapter3.pdf    chapters4web/SIGCOMMeBook2013v1_chapter5.pdf
+cp chapter4/chapter4.pdf    chapters4web/SIGCOMMeBook2013v1_chapter7.pdf
+cp chapter5/chapter5.pdf    chapters4web/SIGCOMMeBook2013v1_chapter2.pdf
+cp chapter6/chapter6.pdf    chapters4web/SIGCOMMeBook2013v1_chapter6.pdf
+cp chapter7/chapter7.pdf    chapters4web/SIGCOMMeBook2013v1_chapter1.pdf
+cp chapter8/chapter8.pdf    chapters4web/SIGCOMMeBook2013v1_chapter3.pdf
+cp chapter9/chapter9.pdf    chapters4web/SIGCOMMeBook2013v1_chapter9.pdf
